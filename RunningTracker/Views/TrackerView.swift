dda6d39d603cdbb61c5e8ddb058e0c95a5c0e143
Aidan Kang <akang@lcs.on.ca>
2023-02-04 21:43:49 -0500

    Started static layout for tracker user interface

--- RunningTracker/Views/TrackerView.swift
+++ RunningTracker/Views/TrackerView.swift
@@ -9,8 +9,59 @@ import SwiftUI
 
 struct TrackerView: View {
     var body: some View {
-        VStack {
-            Text("Tracker goes here soon")
+        VStack(spacing: 30) {
+            
+            VStack(spacing: 15) {
+                HStack(spacing: 30) {
+                    Image(systemName: "figure.run")
+                        .font(.system(size: 80))
+                    Image(systemName: "figure.run")
+                        .font(.system(size: 80))
+                    Image(systemName: "figure.run")
+                        .font(.system(size: 80))
+                }
+                .padding()
+                
+                HStack {
+                    Text("Enter the statistics of your run:")
+                        .font(.title3.smallCaps())
+                        .bold()
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Body Weight (kg):")
+                        .font(.body.smallCaps())
+                    TextField("00.0", text: .constant(""))
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Distance (km):")
+                        .font(.body.smallCaps())
+                    TextField("00.0", text: .constant(""))
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Time (min):")
+                        .font(.body.smallCaps())
+                    TextField("00.0", text: .constant(""))
+                }
+                .padding(.horizontal)
+            }
+            
+            
+            HStack {
+                Text("Your run:")
+                    .font(.title3.smallCaps())
+                    .bold()
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Spacer()
         }
         .navigationTitle("Running Tracker")
     }
