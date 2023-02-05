0dee48674af454db50ac559ef8dc1c8e22e040dd
Aidan Kang <akang@lcs.on.ca>
2023-02-04 20:54:20 -0500

    Initial project organization

---
+++ RunningTracker/Views/TrackerView.swift
@@ -0,0 +1,20 @@
+//
+//  TrackerView.swift
+//  RunningTracker
+//
+//  Created by Aidan Kang on 2023-02-04.
+//
+
+import SwiftUI
+
+struct TrackerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackerView()
+    }
+}
