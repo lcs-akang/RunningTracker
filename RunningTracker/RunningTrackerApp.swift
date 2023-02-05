671ce5da404219efbbb4a14892af1b2c7c1c92ea
Aidan Kang <akang@lcs.on.ca>
2023-02-04 20:05:34 -0500

    Initial Commit

---
+++ RunningTracker/RunningTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningTrackerApp.swift
+//  RunningTracker
+//
+//  Created by Aidan Kang on 2023-02-04.
+//
+
+import SwiftUI
+
+@main
+struct RunningTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
