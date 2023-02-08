8168e8fb8a9e361196ee8128a467f173a8da25a5
Aidan Kang <akang@lcs.on.ca>
2023-02-08 11:48:03 -0500

    Added Result Model file to create structure for a result

---
+++ RunningTracker/Model/Result.swift
@@ -0,0 +1,24 @@
+//
+//  Result.swift
+//  RunningTracker
+//
+//  Created by Aidan Kang on 2023-02-08.
+//
+
+import Foundation
+
+struct Result: Identifiable {
+    
+    let id = UUID()
+    let caloriesBurned: String
+    let runningPace: String
+    let averageSpeed: String
+    let time: String
+    let distance: String
+}
+
+var resultForPreview = Result(caloriesBurned: "315",
+                              runningPace: "6.0",
+                              averageSpeed: "10.0",
+                              time: "30",
+                              distance: "5.0")
