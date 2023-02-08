e95ef27dfd79116e2ac63f57ebac3d59cf876516
Aidan Kang <akang@lcs.on.ca>
2023-02-08 12:03:32 -0500

    finished inteface for ResultView

--- RunningTracker/Views/ResultView.swift
+++ RunningTracker/Views/ResultView.swift
@@ -17,44 +17,73 @@ struct ResultView: View {
     var body: some View {
         
         HStack {
-            VStack {
-                
-                HStack(spacing: 5) {
-                    Image(systemName: "flame.fill")
-                        .font(.system(size: 20))
-                    Text("Calories Burned")
-                        .font(.body.smallCaps())
-                }
-                
-                HStack(spacing: 5) {
-                    Image(systemName: "figure.run")
-                        .font(.system(size: 20))
-                    Text("Running Pace")
-                        .font(.body.smallCaps())
-                }
-                
-                HStack(spacing: 5) {
-                    Image(systemName: "speedometer")
-                        .font(.system(size: 20))
-                    Text("Average Speed")
-                        .font(.body.smallCaps())
-                }
+            VStack(spacing: 3) {
                 
                 HStack(spacing: 5) {
                     Image(systemName: "clock")
                         .font(.system(size: 20))
                     Text("Time")
                         .font(.body.smallCaps())
+                    Spacer()
                 }
+                
                 
                 HStack(spacing: 5) {
                     Image(systemName: "space")
                         .font(.system(size: 20))
                     Text("Distance")
                         .font(.body.smallCaps())
+                    Spacer()
                 }
                 
+                HStack {
+                    Text("============")
+                    Spacer()
+                }
+                
+                HStack(spacing: 5) {
+                    Image(systemName: "flame.fill")
+                        .font(.system(size: 20))
+                    Text("Calories Burned")
+                        .font(.body.smallCaps())
+                    Spacer()
+                }
+                
+                
+                HStack(spacing: 5) {
+                    Image(systemName: "figure.run")
+                        .font(.system(size: 20))
+                    Text("Running Pace")
+                        .font(.body.smallCaps())
+                    Spacer()
+                }
+                
+                
+                HStack(spacing: 5) {
+                    Image(systemName: "speedometer")
+                        .font(.system(size: 20))
+                    Text("Average Speed")
+                        .font(.body.smallCaps())
+                    Spacer()
+                }
+                
+                
             }
+            .padding(.horizontal)
+            
+            Spacer()
+            
+            VStack(spacing: 3) {
+                Text("\(priorResult.time) min")
+                Text("\(priorResult.distance) km")
+                
+                Text("========")
+                
+                Text("\(priorResult.caloriesBurned) kcal")
+                Text("\(priorResult.runningPace) min/km")
+                Text("\(priorResult.averageSpeed) km/h")
+            }
+            .padding(.horizontal)
         }
         
     }
